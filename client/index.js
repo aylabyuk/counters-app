b8a70e1adbcd08963ff2460378633a8415a8fe0e
Oriel Absin <oriel.absin@gmail.com>
2019-07-05 09:47:38 +0800

    Create observable middleware in redux

--- client/index.js
+++ client/index.js
@@ -1,17 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import { createEpicMiddleware } from "redux-observable";
 
 import counterReducer from "./features/counter/reducers";
+import App from "./components/App";
+
+const observableMiddleware = createEpicMiddleware();
 
 const store = createStore(
   counterReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(observableMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
-
-import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
