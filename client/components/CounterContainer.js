2df698e4de1c671239f79f9279ec553d2a666f92
Oriel Absin <oriel.absin@gmail.com>
2019-07-06 21:02:11 +0800

    Add react spring

--- client/components/CounterContainer.js
+++ client/components/CounterContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import { useTransition, animated } from "react-spring";
 
 import CreateCounter from "./CreateCounter";
 import CounterList from "./CounterList/CounterList";
@@ -18,6 +19,48 @@ const StyledRoot = styled.div`
   padding: 50px;
 `;
 
+const StyledContentContainer = styled.div`
+  width: 100%;
+  font-size: 1em;
+  border: 1px solid rgb(232, 232, 232);
+  border-radius: 4px;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  margin: 5px 0px;
+
+  ::-webkit-scrollbar-button {
+    display: block;
+    height: 5px;
+    background-color: #aaa;
+  }
+  ::-webkit-scrollbar-button:hover {
+    background-color: #aaa;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #acacac;
+    border-radius: 5px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #535353;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #efefef;
+  }
+  ::-webkit-scrollbar-track:hover {
+    background-color: #ccc;
+  }
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+`;
+
+const StyledMessage = styled.div`
+  color: gray;
+  text-align: center;
+  padding: 30px;
+`;
+
 const CounterContainer = props => {
   useEffect(() => {
     props.onGetCounters();
@@ -29,21 +72,48 @@ const CounterContainer = props => {
       return { count: a.count + b.count };
     }).count;
 
+  const transitions = useTransition(props.counters.length > 0, p => p, {
+    initial: { opacity: 1, transform: "translate3d(0,0%,0)" },
+    from: { opacity: 0, transform: "translate3d(0,100%,0)" },
+    enter: { opacity: 1, transform: "translate3d(0,0%,0)" },
+    leave: { opacity: 0, transform: "translate3d(0,-50%,0)" },
+    trail: 10
+  });
+
+  const pages = [
+    ({ style }) => (
+      <animated.div style={style}>
+        <CounterList counters={props.counters} />
+      </animated.div>
+    ),
+    ({ style }) => (
+      <animated.div style={style}>
+        <StyledMessage>
+          Hi there! There are no counters available. You might want to create
+          one.
+        </StyledMessage>
+      </animated.div>
+    )
+  ];
+
   return (
     <StyledRoot>
       <CreateCounter
         submitting={props.isCreatingCounter}
         createCounter={props.onAddCounter}
       />
-      <CounterList counters={props.counters} />
+      <StyledContentContainer>
+        {transitions.map(({ item, props, key }) => {
+          const Page = item ? pages[0] : pages[1];
+          return <Page key={key} style={props} />;
+        })}
+      </StyledContentContainer>
       <Total total={totalCounts || 0} />
     </StyledRoot>
   );
 };
 
-const mapStateToProps = state => {
-  return state;
-};
+const mapStateToProps = state => state;
 
 const mapActionsToProps = {
   onAddCounter: addCounter,
--- client/components/CounterList/CounterList.js
+++ client/components/CounterList/CounterList.js
@@ -1,68 +1,14 @@
 import React from "react";
-import styled from "styled-components";
 
 import Counter from "./Counter";
 
-const StyledRoot = styled.div`
-  width: 100%;
-  font-size: 1em;
-  border: 1px solid rgb(232, 232, 232);
-  border-radius: 4px;
-  height: 100%;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  margin: 5px 0px;
-
-  ::-webkit-scrollbar-button {
-    display: block;
-    height: 5px;
-    background-color: #aaa;
-  }
-  ::-webkit-scrollbar-button:hover {
-    background-color: #aaa;
-  }
-  ::-webkit-scrollbar-thumb {
-    background-color: #acacac;
-    border-radius: 5px;
-  }
-  ::-webkit-scrollbar-thumb:hover {
-    background-color: #535353;
-  }
-  ::-webkit-scrollbar-track {
-    background-color: #efefef;
-  }
-  ::-webkit-scrollbar-track:hover {
-    background-color: #ccc;
-  }
-  ::-webkit-scrollbar {
-    width: 8px;
-  }
-`;
-
-const StyledMessage = styled.div`
-  color: gray;
-  text-align: center;
-  padding: 30px;
-`;
-
 const CounterList = props => {
-  return (
-    <StyledRoot>
-      {props.counters.length > 0 ? (
-        props.counters
-          .slice(0)
-          .reverse()
-          .map(counter => {
-            return <Counter key={counter.id} {...counter} />;
-          })
-      ) : (
-        <StyledMessage>
-          Hi there! There are no counters available. You might want to create
-          one.
-        </StyledMessage>
-      )}
-    </StyledRoot>
-  );
+  return props.counters
+    .slice(0)
+    .reverse()
+    .map(counter => {
+      return <Counter key={counter.id} {...counter} />;
+    });
 };
 
 export default CounterList;
